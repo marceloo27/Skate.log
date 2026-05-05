3ede13f7080e0671410fde84d7b26b6be2be17a1
marceloo27 <marcelo.carvalho@sptech.school>
2026-05-05 12:11:59 -0300

    Feat: adicionei mais features pro site, como um quiz só pra entretenimento, adicionei a pasta bd e fiz mais estilos pra dash

---
+++ projeto/banco de dados/skate.logbd.sql
@@ -0,0 +1,58 @@
+DROP DATABASE IF EXISTS `Skate.log`;
+CREATE DATABASE `Skate.log`;
+USE `Skate.log`;
+
+-- 1. Tabela de Usuário 
+CREATE TABLE usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
+    senha VARCHAR(255),
+    estilo VARCHAR(50) default 'street'
+);
+
+-- 2. Tabela de Manobras (Catálogo fixo para o skatista escolher)
+CREATE TABLE manobra (
+    idManobra INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    categoria VARCHAR(45) -- 'Street' ou 'Transition'
+);
+
+-- 3. Tabela de Sessão 
+CREATE TABLE sessao (
+    idSessao INT PRIMARY KEY AUTO_INCREMENT,
+    fkUsuario INT,
+    dtSessao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    tempoMinutos INT,
+    diaSemana VARCHAR(15),
+    CONSTRAINT fk_sessao_usuario FOREIGN KEY (fkUsuario) 
+        REFERENCES usuario(idUsuario) ON DELETE CASCADE
+);
+
+-- 4. Tabela de Registro (Quais manobras foram feitas em qual treino)
+CREATE TABLE registro_manobra (
+    idRegistro INT PRIMARY KEY AUTO_INCREMENT,
+    fkSessao INT,
+    fkManobra INT,
+    CONSTRAINT fk_reg_sessao FOREIGN KEY (fkSessao) 
+        REFERENCES sessao(idSessao) ON DELETE CASCADE,
+    CONSTRAINT fk_reg_manobra FOREIGN KEY (fkManobra) 
+        REFERENCES manobra(idManobra)
+);
+
+-- INSERTs Básicos para ter o que mostrar no site
+INSERT INTO manobra (nome, categoria) VALUES 
+('Ollie', 'Street'), ('Kickflip', 'Street'), ('Heelflip', 'Street'),
+('Drop-in', 'Transition'), ('Rock to Fakie', 'Transition'), ('Manual', 'Street');
+
+select * from usuario;
+
+select * from sessao;
+
+select * from registro_manobra;
+
+select fkManobra, count(*) from registro_manobra r join manobra m on r.fkManobra = m.idManobra group by fkManobra;
+
+
+
+
